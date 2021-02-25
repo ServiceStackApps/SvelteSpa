3f6b36888f51d34cb3fdbc6e3607c0f7cc5d3858
Darren Reid <darren@reidmail.org>
2021-02-25 16:17:46 +1100

    first commit

---
+++ SvelteSpaTemplate.ServiceModel/Hello.cs
@@ -0,0 +1,18 @@
+using ServiceStack;
+
+namespace SvelteSpaTemplate.ServiceModel
+{
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+}
---
+++ SvelteSpaTemplate/src/App.svelte
@@ -0,0 +1,46 @@
+<svelte:window on:pushstate={x => pathname = x.target.location.pathname} on:popstate={x => pathname = x.target.location.pathname}/>
+
+<script lang="ts">
+import { Router, Route, link } from 'svelte-routing';
+import { routes, routeLinks } from './shared/routes';
+import { checkAuth } from './shared';
+
+let pathname = location.pathname;
+
+let links = null;
+routeLinks.subscribe(x => links = x);
+
+checkAuth();
+</script>
+
+<Router>
+<nav class="navbar navbar-expand-lg navbar-dark">
+	<div class="container">
+		<a class="navbar-brand" href="/">
+			Svelte Spa Template
+		</a>
+		<div class="navbar-collapse">
+			<ul class="navbar-nav mr-auto">
+			{#each links as {path, label, exact} (label) }
+				<li class="nav-item" class:active={exact ? pathname == path : pathname.startsWith(path)}>
+					<a href="{path}" class="nav-link" use:link>{label}</a>
+				</li>
+			{/each}
+			</ul>
+		</div>
+	</div>
+</nav>
+<div class="container">
+	<div class="row p-4">
+		<div id="content">
+			{#each routes as {path,component}}
+				<Route path="{path}" component="{component}" />
+			{/each}
+		</div>
+	</div>
+</div>
+
+<h4 style="position:absolute; bottom: 20px; width: 100%; text-align: center">
+	<a href="https://github.com/NetCoreTemplates/svelte-spa">Learn about this Svelte Project Template</a>
+</h4>
+</Router>
